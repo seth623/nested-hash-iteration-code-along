d0d80ad15a40ddd2c73049676f353925211f0b7e
seth <seth623@gmail.com>
2022-01-08 18:27:03 -0700

    Completed Assignment

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,4 +16,15 @@ require 'pry'
   
 def remove_strawberry(contacts)
 
+  contacts.each do |contact, contact_details_hash|
+    if contact == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors 
+          data.delete_if { |ice_cream| ice_cream == "strawberry" }
+        end  
+      end 
+    end 
+  end  
+
 end
+ 
